91b67370a59af76d8776e4e171e7481b958e0103
BVN4 <ruslan2804y@gmail.com>
2022-01-25 14:28:19 +0300

    Creation of Krasivsk

--- commands/activities.js
+++ commands/activities.js
@@ -10,35 +10,34 @@ module.exports = {
 
 	slashOptions : [
 		{
-			type:7,
-			name: 'канал',
-			description: 'Выберите голосовой канал',
-			required: true,
-			channel_types: [ 2 ]
+			type :7,
+			name : 'канал',
+			description : 'Выберите голосовой канал',
+			required : true,
+			channel_types : [ 2 ]
 		},
 		{
-		type: 3, 
-		name: 'activity',
-		description:'Выберите activity',
-		required: true,
-		choices: [
-			{'name': "Watch together",          'value': '880218394199220334'},
-			{'name': "Poker Night",             'value': '755827207812677713'},
-			{'name': "Betrayal.io",             'value': '773336526917861400'},
-			{'name': "Fishington.io",           'value': '814288819477020702'},
-			{'name': "Chess In The Park",       'value': '832012774040141894'},
-			{'name': "Sketchy Artist",          'value': '879864070101172255'},
-			{'name': "Awkword",                 'value': '879863881349087252'},
-			{'name': "Putts",                   'value': '832012854282158180'},
-			{'name': "Doodle Crew",             'value': '878067389634314250'},
-			{'name': "Letter Tile",             'value': '879863686565621790'},
-			{'name': "Word Snacks",             'value': '879863976006127627'},
-			{'name': "SpellCast",               'value': '852509694341283871'},
-			{'name': "Checkers In The Park",    'value': '832013003968348200'},
-			{'name': "CG4 Prod",                'value': '832025144389533716'}
+			type : 3,
+			name : 'activity',
+			description :'Выберите activity',
+			required : true,
+			choices : [
+				{ value : '880218394199220334', name : "Watch together" },
+				{ value : '755827207812677713', name : "Poker Night" },
+				{ value : '773336526917861400', name : "Betrayal.io" },
+				{ value : '814288819477020702', name : "Fishington.io" },
+				{ value : '832012774040141894', name : "Chess In The Park" },
+				{ value : '879864070101172255', name : "Sketchy Artist" },
+				{ value : '879863881349087252', name : "Awkword" },
+				{ value : '832012854282158180', name : "Putts" },
+				{ value : '878067389634314250', name : "Doodle Crew" },
+				{ value : '879863686565621790', name : "Letter Tile" },
+				{ value : '879863976006127627', name : "Word Snacks" },
+				{ value : '852509694341283871', name : "SpellCast" },
+				{ value : '832013003968348200', name : "Checkers In The Park" },
+				{ value : '832025144389533716', name : "CG4 Prod" }
 			]
 		}
-
 	],
 
 	init : function(){ return this; },
