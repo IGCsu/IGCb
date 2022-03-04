855daade8ca585fcb7dae34d2dbb167392132ed8
TheKing-OfTime <artem.matvienko0@gmail.com>
2022-03-04 22:37:36 +0300

    Fix varible logic. Prepare to result button

--- commands/poll.js
+++ commands/poll.js
@@ -82,14 +82,20 @@ module.exports = {
 							{
 								type : 2,
 								style: 3,
-								customId:`poll|yes|${min}`,
+								customId:`poll|yes|${min}|${type}`,
 								label:'За'
 							},
 							{
 								type : 2,
 								style: 4,
-								customId:`poll|no|${min}`,
+								customId:`poll|no|${min}|${type}`,
 								label:'Против'
+							},
+							{
+								type : 2,
+								style: 2,
+								customId:`poll|result|${min}|${type}`,
+								label:'Результаты'
 							}
 						]
 					}
@@ -102,12 +108,15 @@ module.exports = {
 
     button : async function(int){
         //int.reply({content: 'В разработке', ephemeral: true});
-		const type = int.customId.split('|')[1]
+		const resp = int.customId.split('|')[1]
+		const type = int.customId.split('|')[3]
 		if(type == 'senate' && (int.member.roles.cache.get('916999822693789718') || int.member.roles.cache.get('613412133715312641'))){
 			await int.reply({content: 'Отказано в достпуе', ephemeral: true})
 		}
 
 		const min = int.customId.split('|')[2];
+
+		if(resp == 'result') return int.reply({content: 'В разработке', ephemeral: true});
 
 		await client.api.interactions(int.id, int.token).callback.post({
 			data:{
@@ -120,7 +129,7 @@ module.exports = {
 						components:[{
 							type: 4,
 							custom_id: 'opininon',
-							label: 'Почему вы выбрали именно \"' + ((type == 'yes') ? 'За': 'Против') + '\"',
+							label: 'Почему вы выбрали именно \"' + ((resp == 'yes') ? 'За': 'Против') + '\"',
 							style: 2,
 							min_length: min == 'none' ? 25 : min,
 							max_length: 1000,
