8ea3fb33e51bb92f8b809922c579d402bc12238d
BVN4 <ruslan2804y@gmail.com>
2022-05-23 19:47:55 +0300

    Update activities.js

--- commands/activities/index.js
+++ commands/activities/index.js
@@ -13,23 +13,42 @@ module.exports = {
 
 	init : function(){ return this; },
 
+
 	/**
-	 * Выставляет активность указанному каналу
+	 * Генерирует и отправляет приглашение к голосовому каналу с выбранным Activity
 	 *
 	 * @param {CommandInteraction} int Команда пользователя
 	*/
 	slash : async function(int){
-		const invite = await client.api.channels(int.options.get('channel').value).invites.post({
-			data : { target_type : 2, target_application_id : int.options.get('activity').value }
+
+		const lang = int.locale.split('-')[0];
+
+		const channel = int.options.get('channel').value;
+		const activityId = int.options.get('activity').value;
+
+		const activity = this.slashOptions.activity.choices[activityId];
+		const activityName = activity[lang] ?? activity.ru
+
+		const invite = await client.api.channels(channel).invites.post({
+			data : {
+				target_type : 2,
+				target_application_id : activityId
+			}
 		});
 
 		int.reply({
-			content : `Приглашение сгенерированно, нажмите на кнопку ниже чтобы присоедениться к activity.`,
+			content : 'Приглашение сгенерированно, нажмите на кнопку ниже чтобы присоедениться к ' + activityName,
 			components : [{
 				type : 1,
-				components : [{ type : 2, style : 5, url : 'https://discord.gg/' + invite.code, label : 'Присоединиться'}]
+				components : [{
+					type : 2,
+					style : 5,
+					url : 'https://discord.gg/' + invite.code,
+					label : 'Присоединиться'
+				}]
 			}]
 		});
+
 	},
 
 };
