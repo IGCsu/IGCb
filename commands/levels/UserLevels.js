d0e184ff6ebc4aeebf6aade8bb94b592289df8ab
BVN4 <ruslan2804y@gmail.com>
2022-05-27 10:55:58 +0300

    Add UserLevels class into levels

---
+++ commands/levels/UserLevels.js
@@ -0,0 +1,426 @@
+class UserLevels {
+
+	/**
+	 * Содержит true - если пользователь был найден в базе
+	 * @type {Boolean}
+	 */
+	finded = false;
+
+	/**
+	 * Массив уровней
+	 * @type {Array}
+	 */
+	#roles = [];
+
+	/**
+	 * Массив ID ролей уровней. Используется для поиска.
+	 * @type {Array}
+	 */
+	#rolesIDs = [];
+
+	/**
+	 * Содержит объект примитивных данных пользователя
+	 * @type {Object}
+	 */
+	#primitiveData = {};
+
+	/**
+	 * Содержит объект продвинутых данных пользователя
+	 * @type {Object}
+	 */
+	#advancedData = {};
+
+	/**
+	 * Содержит эмбед данных
+	 * @type {MessageEmbed}
+	 */
+	#embed = undefined;
+
+	/**
+	 * Получение пользователя из БД.
+	 * @param  {GuildMember} member
+	 * @param  {Array}       roles    Массив уровней
+	 * @param  {Array}       rolesIDs Массив ID ролей уровней.
+	 * @param  {Boolean}     create   Если true - пользователь будет создан в базе, если не будет найден
+	 * @return {Object}               Объект пользователя
+	 */
+	constructor(member, roles, rolesIDs, create){
+
+		this.member = member;
+		this.#roles = roles;
+		this.#rolesIDs = rolesIDs;
+
+		const users = DB.query('SELECT * FROM levels WHERE id = ?', [this.member.id]);
+
+		if(users[0]){
+			this.finded = true;
+			this.#primitiveData = {
+				messagesLegit : users[0].messagesLegit,
+				messagesAll : users[0].messagesAll,
+				activity : users[0].activity,
+				symbols : users[0].symbols,
+				last : users[0].last
+			};
+		}else if(create){
+			DB.query('INSERT INTO levels (`id`) VALUES (?)', [this.member.id]);
+			this.#primitiveData = {
+				messagesLegit : 0,
+				messagesAll : 0,
+				activity : 1,
+				symbols : 0,
+				last : 0
+			};
+		}
+
+	};
+
+	/**
+	 * Обновляет данные пользователя в базе данных
+	 */
+	update(){
+		DB.query('UPDATE levels SET messagesAll = ?, messagesLegit = ?, symbols = ?, last = ? WHERE id = ?', [
+			this.#primitiveData.messagesAll,
+			this.#primitiveData.messagesLegit,
+			this.#primitiveData.symbols,
+			this.#primitiveData.last,
+			this.member.id
+		]);
+
+		return this;
+	};
+
+	/**
+	 * Регистрирует новое сообщение пользователя.
+	 * После регистрации обновляются примитивные данные, но продвинутые теряют свою актуальность, потому очищаются.
+	 * @param {Message} msg Сообщение пользователя
+	 */
+	userMessageСounting(msg){
+		const timestamp = Math.floor(msg.createdTimestamp / 1000);
+
+		this.#primitiveData.messagesAll += 1;
+		this.#primitiveData.symbols += msg.content.length;
+
+		if(this.#primitiveData.last + 60 <= timestamp){
+			this.#primitiveData.last = timestamp;
+			this.#primitiveData.messagesLegit += 1;
+		}
+
+		this.#advancedData = {};
+		this.#embed = undefined;
+
+		return this;
+	};
+
+	/**
+	 * Обновляет роль пользователя в дискорде, если требуется
+	 */
+	updateRole(){
+		if(this.member.id == '256114365894230018') return;
+
+		const role = this.getRole();
+
+		if(this.member.roles.cache.has(role.id)) return;
+
+		if(role.id != '648762974277992448')
+			this.member.roles.add(role.cache, 'По причине изменения уровня');
+
+		this.member.roles.cache.filter(r => this.#rolesIDs.includes(r.id)).each(r => {
+			if(r.id != role.id) this.member.roles.remove(r, 'По причине изменения уровня');
+		});
+
+		return this;
+	};
+
+
+
+
+
+	/**
+	 * ***************************************************************************
+	 * Функции возвращения примитивных данных
+	 * ***************************************************************************
+	 */
+
+	/**
+	 * Возвращает количество всех сообщений пользователя
+	 * @return {Number}
+	 */
+	getMessagesAll(user){
+		return this.#primitiveData.messagesAll;
+	};
+
+	/**
+	 * Возвращает количество только засчитанных сообщений пользователя
+	 * @return {Number}
+	 */
+	getMessagesLegit(user){
+		return this.#primitiveData.messagesLegit;
+	};
+
+	/**
+	 * Возвращает количество старых сообщений пользователя, которые не учувствуют в подсчёте
+	 * @return {Number}
+	 */
+	getMessagesOld(user){
+		return this.#primitiveData.messagesOld;
+	};
+
+	/**
+	 * Возвращает количество всех символов пользователя
+	 * @return {Number}
+	 */
+	getSymbols(user){
+		return this.#primitiveData.symbols;
+	};
+
+	/**
+	 * Возвращает количество активных дней пользователя за последние 30 суток
+	 * @return {Number}
+	 */
+	getActivity(user){
+		return this.#primitiveData.activity;
+	};
+
+
+
+
+
+	/**
+	 * ***************************************************************************
+	 * Функции возвращения продвинутых данных
+	 * ***************************************************************************
+	 */
+
+	/**
+	 * Возвращает процент оверпоста
+	 * @return {Number}
+	 */
+	getOverpost(){
+		if(this.#advancedData.overpost) return this.#advancedData.overpost;
+
+		const messagesAll = this.getMessagesAll();
+		const messagesLegit = this.getMessagesLegit();
+
+		const overpost = Math.round( (messagesAll - messagesLegit) / messagesLegit * 1000) / 10;
+
+		return this.#advancedData.overpost = isNaN(overpost) ? 0 : overpost;
+	};
+
+	/**
+	 * Возвращает среднее количество символов в сообщениях
+	 * @return {Number}
+	 */
+	getSymbolsAvg(){
+		if(this.#advancedData.symbolsAvg) return this.#advancedData.symbolsAvg;
+
+		const messagesAll = this.getMessagesAll();
+		const symbols = this.getSymbols();
+
+		const symbolsAvg = Math.round( (symbols / messagesAll) * 10) / 10;
+
+		return this.#advancedData.symbolsAvg = isNaN(symbolsAvg) ? 0 : symbolsAvg;
+	};
+
+	/**
+	 * Возвращает процент активности
+	 * @return {Number}
+	 */
+	getActivityPer(){
+		if(this.#advancedData.activityPer) return this.#advancedData.activityPer;
+
+		const activity = this.getActivity();
+
+		return this.#advancedData.activityPer = Math.round( activity / 30 * 1000 ) / 10;
+	};
+
+	/**
+	 * Возвращает опыт
+	 * @return {Number}
+	 */
+	getExp(){
+		if(this.#advancedData.exp) return this.#advancedData.exp;
+
+		const messagesLegit = this.getMessagesLegit();
+		const symbolsAvg = this.getSymbolsAvg();
+		const activityPer = this.getActivityPer();
+
+		const exp = Math.round(messagesLegit * symbolsAvg / 100 * activityPer);
+
+		return this.#advancedData.exp = isNaN(exp) ? 0 : exp;
+	};
+
+	/**
+	 * Возвращает количество оштрафованного опыта пользователя
+	 * @return {Number}
+	 */
+	getExpFine(){
+		if(this.#advancedData.expFine) return this.#advancedData.expFine;
+
+		const activityPer = this.getActivityPer();
+		const exp = this.getExp();
+
+		const expFine = Math.round(100 / activityPer * exp - exp);
+
+		return this.#advancedData.expFine = isNaN(expFine) ? 0 : expFine;
+	};
+
+	/**
+	 * Возвращает роль пользователя
+	 * @return {Object}
+	 */
+	getRole(){
+		if(this.#advancedData.role) return this.#advancedData.role;
+
+		const exp = this.getExp();
+
+		for(const role of this.#roles)
+			if(role.value <= exp) return this.#advancedData.role = role;
+	};
+
+	/**
+	 * Возвращает следующую роль пользователя. Возвращает true - если следующей роли нет
+	 * @return {Object}
+	 */
+	getNextRole(){
+		if(this.#advancedData.nextRole) return this.#advancedData.nextRole;
+
+		const exp = this.getExp();
+		const role = this.getRole();
+
+		return this.#advancedData.nextRole = this.#roles[role.pos - 1] ?? true;
+	};
+
+	/**
+	 * Возвращает прогресс до следующей роли. Возвращает true - если следующей роли нет
+	 * @return {Number}
+	 */
+	getNextRoleProgress(user){
+		if(this.#advancedData.nextRoleProgress) return this.#advancedData.nextRoleProgress;
+
+		const exp = this.getExp();
+		const role = this.getRole();
+		const nextRole = this.getNextRole();
+
+		if(nextRole == true) return true;
+
+		const nextRoleProgress = Math.round( ( (exp - role.value) / (nextRole.value - role.value) ) * 1000) / 10;
+
+		return this.#advancedData.nextRoleProgress = nextRoleProgress;
+	};
+
+
+
+
+
+	/**
+	 * ***************************************************************************
+	 * Функции возвращения эмбеда
+	 * ***************************************************************************
+	 */
+
+	/**
+	 * Генерирует эмбед с данными пользователя
+	 * @return {MessageEmbed}
+	 */
+	getEmbed(){
+		if(this.#embed) return this.#embed;
+
+		this.#embed = new Discord.MessageEmbed();
+
+		this.#embed.setTitle('Статистика пользователя');
+		this.#embed.setThumbnail(this.member.user.avatarURL({ dynamic: true }));
+		this.#embed.setDescription(this.member.toString());
+
+		this.addMessages();
+		this.addSymbols();
+		this.addOverpost();
+		this.addActivity();
+		this.addExp();
+		this.addNextRole();
+
+		this.setColor();
+
+		return this.#embed;
+	};
+
+	/**
+	 * Добавляет к эмбеду статистику сообщений
+	 */
+	addMessages(){
+		const messagesAll = this.getMessagesAll().toLocaleString();
+		const messagesLegit = this.getMessagesLegit().toLocaleString();
+
+		this.#embed.addField('Cообщения:', messagesAll + ' (Из них учитываются: ' + messagesLegit + ')');
+	};
+
+	/**
+	 * Добавляет к эмбеду статистику символов
+	 */
+	addSymbols(){
+		const symbols = this.getSymbols().toLocaleString();
+		const symbolsAvg = this.getSymbolsAvg().toLocaleString();
+
+		this.#embed.addField('Cимволы:', symbols + ' (AVG ' + symbolsAvg + ')');
+	};
+
+	/**
+	 * Добавляет к эмбеду показатель оверпоста
+	 */
+	addOverpost(){
+		const overpost = this.getOverpost();
+
+		this.#embed.addField('Оверпост:', overpost + '%');
+	};
+
+	/**
+	 * Добавляет к эмбеду показатель активности
+	 */
+	addActivity(){
+		const activity = this.getActivity();
+		const activityPer = this.getActivityPer();
+
+		if(activityPer == 100) return;
+
+		this.#embed.addField('Активность за последние 30 дней:', activityPer + '% (' + activity + '/' + '30)');
+	};
+
+	/**
+	 * Добавляет к эмбеду количество опыта
+	 */
+	addExp(){
+		const exp = this.getExp().toLocaleString();
+		const activityPer = this.getActivityPer();
+		const expFine = this.getExpFine();
+
+		let text = exp;
+		if(expFine) text += ' (Вычтено из за неактивности: ' + expFine.toLocaleString() + ')';
+
+		this.#embed.addField('Опыт:', text, activityPer == 100);
+	};
+
+	/**
+	 * Добавляет к эмбеду следующую роль и прогресс до неё, если есть
+	 */
+	addNextRole(){
+		const role = this.getRole();
+		const nextRole = this.getNextRole();
+		const nextRoleProgress = this.getNextRoleProgress();
+
+		let text = nextRole ? '🎉' : nextRole.cache.toString() + ' ' + nextRoleProgress + '%';
+
+		this.#embed.addField('Прогресс:', role.cache.toString() + ' -> ' + text);
+	};
+
+	/**
+	 * Устанавливает у эмбеда цвет текущей роли пользователя
+	 */
+	setColor(){
+		const role = this.getRole();
+
+		this.#embed.setColor(role.cache.color);
+	};
+
+
+}
+
+module.exports = UserLevels
--- commands/levels/index.js
+++ commands/levels/index.js
@@ -1,7 +1,10 @@
 const fetch = require('node-fetch');
+
 const slashOptions = require('./slashOptions.json');
 const { title, description } = require('./about.json');
 const noXPChannels = require('./noXPChannels.json');
+const UserLevels = require('./UserLevels');
+
 const { command } = require('../help');
 
 module.exports = {
@@ -15,6 +18,18 @@ module.exports = {
 	slashOptions : slashOptions,
 
 	noXPChannels : noXPChannels,
+
+	/**
+	* Массив уровней
+	* @type {Array}
+	*/
+	roles : [],
+
+	/**
+	* Массив ID ролей уровней. Используется для поиска.
+	* @type {Array}
+	*/
+	rolesIDs : [],
 
 	init : async function(path){
 
@@ -37,56 +52,20 @@ module.exports = {
 	/**
 	 * Обработка команды
 	 * Выдаёт статистику по пользовтаелю и ссылку на страницу
-	 * @param {CommandInteraction|UserContextMenuInteraction} int    Команда пользователя
-	 * @param {GuildMember}                                   member Объект пользователя
+	 * @param  {CommandInteraction|UserContextMenuInteraction} int    Команда пользователя
+	 * @param  {GuildMember}                                   member Объект пользователя
+	 * @return {InteractionReplyOptions}
 	 */
 	call : async function(int, member){
 
 		const name = member2name(member, true);
-		const user = this.getUser(member.user.id);
+		const user = new UserLevels(member, this.roles, this.rolesIDs);
 
-		if(!user) return { error: 'Unknown User' };
+		if(!user.finded) return { error: 'Unknown User' };
 
-		let embed = new Discord.MessageEmbed()
-			.setTitle('Статистика пользователя')
-			.setThumbnail(member.user.avatarURL({ dynamic: true }))
+		const embed = user.getEmbed();
 
-		try {
-			embed.setDescription('<@' + user.id + '>');
-			embed.addField('Cообщения:',
-				user.messagesAll.toLocaleString() + ' (Из них учитываются: ' + user.messagesLegit.toLocaleString() + ')');
-			embed.addField('Cимволы:',
-				user.symbols.toLocaleString() + '  (AVG ' + (user.symbolsAvg = this.getSymbolsAvg(user)).toLocaleString() + ')');
-			embed.addField('Оверпост:', (user.overpost = this.getOverpost(user)) + '%');
-			user.activityPer = this.getActivityPer(user);
-			if(user.activityPer != 100)
-			embed.addField('Активность за последние 30 дней:',
-				(user.activityPer = this.getActivityPer(user)) + '% (' + Math.round(user.activityPer * 30/100) + '/' + '30)');
-
-			user.expFine = this.getExpFine(user);
-
-			embed.addField('Опыт:',
-				(user.exp = this.getExp(user)).toLocaleString() +
-				(user.expFine ? (' (Вычтено из за неактивности: ' + user.expFine.toLocaleString() + ')') : ''), (user.activityPer == 100));
-
-			user.nextRole = this.getNextRole(user);
-
-			let txt = '🎉'
-			if(user.nextRole != true){
-				txt = '<@&' + user.nextRole.cache.id + '> ' + (user.nextRoleProgress = this.getNextRoleProgress(user)) + '%'
-			};
-
-			embed.addField('Прогресс:', 
-				'<@&' + user.role.cache.id + '> -> ' + txt)
-			
-			embed.setColor(user.role.cache.color);
-
-		}catch(e){
-			console.error(int, member, user, embed, e.toString());
-			return { error: 'Can\'t resolve the user data' };
-		};
-
-		const status = commands.nocommand.siteOffline;
+		const status = commands.nocommand?.siteOffline;
 
 		return {
 			embeds : [embed],
@@ -130,200 +109,19 @@ module.exports = {
 
 	/**
 	 * Мониторинг всех сообщений для начисления опыта пользователям. Игнорируются сообщения бота и в некоторых каналах.
-	 * @param  {Message} msg Сообщение пользователя
+	 * @param {Message} msg Сообщение пользователя
 	 */
 	monitoring : async function(msg){
 		if(msg.author.bot) return;
 		if(this.noXPChannels.includes(msg.channel.parentId)) return;
 		if(this.noXPChannels.includes(msg.channelId)) return;
 
-		let user = this.getUser(msg.author.id, true);
-		user = this.userMessageСounting(user, msg);
+		let user = new UserLevels(msg.member, this.roles, this.rolesIDs, true);
 
-		if(msg.author.id == '256114365894230018') return;
+		user.userMessageСounting(msg)
+			.update()
+			.updateRole();
 
-		const role = this.getRole(user);
-		if(msg.member.roles.cache.has(role.id)) return;
-
-		// try{
-			if(role.id != '648762974277992448')
-				msg.member.roles.add(role.cache, 'По причине изменения уровня');
-
-			msg.member.roles.cache.filter(r => this.rolesIDs.includes(r.id)).each(r => {
-				if(r.id != role.id) msg.member.roles.remove(r, 'По причине изменения уровня');
-			});
-		// }catch(e){}
-
-	},
-
-
-	/**
-	 * Получение пользователя из БД.
-	 * @param  {String}  id     ID пользователя
-	 * @param  {Boolean} create Если true - пользователь будет создан в базе, если не будет найден
-	 * @return {Object}         Объект пользователя
-	 */
-	getUser : (id, create) => {
-
-		const users = DB.query('SELECT * FROM levels WHERE id = ?', [id]);
-		if(users[0]) return users[0];
-
-		if(!create) return false;
-
-		DB.query('INSERT INTO levels (`id`) VALUES (?)', [id]);
-		return { id : id, last : 0, messagesAll : 0, messagesLegit : 0, symbols : 0, activity : 1 };
-
-	},
-
-	/**
-	 * Регистрирует новое сообщение пользователю. Возвращает новый объект. Если обновление не удалось - возвращает старый объект
-	 * @param  {Object}  user Объект пользователя
-	 * @param  {Message} msg  Сообщение пользователя
-	 * @return {Object}       Объект пользователя. Новый, если обновление удалось
-	 */
-	userMessageСounting : (user, msg) => {
-		const timestamp = Math.floor(msg.createdTimestamp / 1000);
-
-		let sql = 'UPDATE levels SET messagesAll = messagesAll + 1, symbols = symbols + ? WHERE id = ?';
-		let dataSql = [msg.content.length, user.id];
-
-		let newUser = {
-			id : user.id,
-			last : user.last,
-			messagesAll : user.messagesAll + 1,
-			messagesLegit : user.messagesLegit,
-			symbols : user.symbols + msg.content.length,
-			activity : user.activity
-		};
-
-		if(user.last + 60 <= timestamp){
-			newUser.last = timestamp;
-			newUser.messagesLegit += 1;
-
-			sql = 'UPDATE levels SET messagesAll = messagesAll + 1, messagesLegit = messagesLegit + 1, symbols = symbols + ?, last = ? WHERE id = ?';
-			dataSql = [msg.content.length, timestamp, user.id];
-		}
-
-		try{
-			DB.query(sql, dataSql);
-			return newUser;
-		}catch(e){
-			console.error('DB error occurred:\n' + e);
-			return user;
-		}
-	},
-
-
-
-
-	/**
-	 * ===========================================================================
-	 * Функции, возвращающие данные пользователя
-	 * ===========================================================================
-	 */
-
-	/**
-	 * Возвращает процент оверпоста
-	 * @param  {Object} user Объект пользователя
-	 * @return {Number}      Показатель оверпоста
-	 */
-	getOverpost : function(user){
-		const overpost = Math.round( (user.messagesAll - user.messagesLegit) / user.messagesLegit * 1000) / 10;
-		return isNaN(overpost) ? 0 : overpost;
-	},
-
-	/**
-	 * Возвращает среднее количество символов в сообщениях
-	 * @param  {Object} user Объект пользователя
-	 * @return {Number}      Среднее количество символов в сообщениях
-	 */
-	getSymbolsAvg : function(user){
-		const symbolsAvg = Math.round( (user.symbols / user.messagesAll) * 10) / 10;
-		return isNaN(symbolsAvg) ? 0 : symbolsAvg;
-	},
-
-	/**
-	 * Возвращает процент активности
-	 * @param  {Object} user Объект пользователя
-	 * @return {Number}      Процент активности
-	 */
-	getActivityPer : function(user){
-		return Math.round(user.activity/30*1000)/10;
-	},
-
-	/**
-	 * Возвращает опыт
-	 * @param  {Object} user             Объект пользователя
-	 * @param  {Number} user.symbolsAvg  Среднее количество символов в сообщениях
-	 * @param  {Number} user.activityPer Процент активности
-	 * @return {Number}                  Опыт
-	 */
-	getExp : function(user){
-		if(!user.symbolsAvg) user.symbolsAvg = this.getSymbolsAvg(user);
-		if(!user.activityPer) user.activityPer = this.getActivityPer(user);
-
-		const exp = Math.round(user.messagesLegit * user.symbolsAvg / 100 * user.activityPer);
-		return isNaN(exp) ? 0 : exp;
-	},
-
-	/**
-	 * Возвращает количество оштрафованного опыта пользователя
-	 * @param  {Object} user             Объект пользователя
-	 * @param  {Number} user.activityPer Процент активности
-	 * @param  {Number} user.exp         Опыт
-	 * @return {Number}                  Оштрафованный опыт
-	 */
-	getExpFine : function(user){
-		if(!user.activityPer) user.activityPer = this.getActivityPer(user);
-		if(!user.exp) user.exp = this.getExp(user);
-
-		const expFine = Math.round(100 / user.activityPer * user.exp - user.exp);
-		return isNaN(expFine) ? 0 : expFine;
-	},
-
-	/**
-	 * Возвращает роль пользователя
-	 * @param  {Object} user     Объект пользователя
-	 * @param  {Number} user.exp Опыт
-	 * @return {Object}          Текущая роль
-	 */
-	getRole : function(user){
-		if(!user.exp) user.exp = this.getExp(user);
-
-		for(const role of this.roles)
-			if(role.value <= user.exp) return role;
-	},
-
-	/**
-	 * Возвращает следующую роль пользователя. Возвращает true - если следующей роли нет
-	 * @param  {Object} user      Объект пользователя
-	 * @param  {Number} user.exp  Опыт
-	 * @param  {Object} user.role Текущая роль
-	 * @return {Object}           Следующая роль
-	 */
-	getNextRole : function(user){
-		if(!user.exp) user.exp = this.getExp(user);
-		if(!user.role) user.role = this.getRole(user);
-
-		return this.roles[user.role.pos - 1] ?? true;
-	},
-
-	/**
-	 * Возвращает прогресс до следующей роли. Возвращает true - если следующей роли нет
-	 * @param  {Object} user          Объект пользователя
-	 * @param  {Number} user.exp      Опыт
-	 * @param  {Object} user.role     Текущая роль
-	 * @param  {Object} user.nextRole Следующая роль
-	 * @return {Number}               Прогресс до следующей роли
-	 */
-	getNextRoleProgress : function(user){
-		if(!user.exp) user.exp = this.getExp(user);
-		if(!user.role) user.role = this.getRole(user);
-		if(!user.nextRole) user.nextRole = this.getNextRole(user);
-
-		if(user.nextRole == true) return true;
-
-		return Math.round(((user.exp - user.role.value)/(user.nextRole.value - user.role.value))*1000)/10;
 	},
 
 };
