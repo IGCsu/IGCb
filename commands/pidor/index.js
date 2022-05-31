9a8606759249d1692dfa90b92bfe24635040df3b
Fedor Lobzun <lobzun@list.ru>
2022-05-31 17:07:16 +0400

    Added timer

--- commands/pidor/index.js
+++ commands/pidor/index.js
@@ -1,4 +1,5 @@
-﻿const { title, description } = require('./about.json');
+﻿const sleep = require('../../functions/sleep');
+const { title, description } = require('./about.json');
 module.exports = {
 
 
@@ -11,21 +12,28 @@ module.exports = {
 	description: description,
 
 
-	init: function () { return this; },
+	init: function () { return this; }, 
+	/**
+	 * out - функция, которая отправляет заданное сообщение с задержкой в 2 секунды
+	 * @param {CommandInteraction} int 
+	 * @param {String} string 
+	 */
+	out: async function(int, string){
+		await int.channel.sendTyping();
+		await sleep(2000);
+		await int.editReply({
+			content: string
+		})
+	},
 
 
-	call: function (int) {
-		int.reply({
+	call: async function (int) {
+		await int.reply({
 			content: 'Игогооооооо!'
 		})
-		
-		int.editReply({
-			content: 'Думаю...'
-		})
-		int.editReply({
-			content: 'А пониеб-то у нас:'
-		})
-		int.followUp({
+		await this.out(int, 'Думаю...');
+		await this.out(int, 'А пониеб-то у нас:');
+		await int.followUp({
 			content: int.guild.members.cache.random().tag + '!'
 		})
 	},
