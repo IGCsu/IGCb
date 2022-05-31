4657f8dacc3b9e17126e8cf3a15c0680acf8bb3d
TheKing-OfTime <artem.matvienko0@gmail.com>
2022-06-01 02:10:05 +0300

    Add sorry message to voice module

--- commands/voice/index.js
+++ commands/voice/index.js
@@ -1,5 +1,5 @@
 const slashOptions = require('./slashOptions.json');
-const { title } = require('./about.json');
+const { title, sorryMessage } = require('./about.json');
 
 module.exports = {
 
@@ -34,10 +34,15 @@ module.exports = {
 			if(c.name == 'Создать канал') {
 				this.channelCreate = c;
 				if(this.channelCreate.members.filter(m => !m.user.bot).size && c.type == 'GUILD_VOICE'){
+					this.channelCreate.members.first().send(sorryMessage['ru']).catch();
 					const channel = await this.create(this.channelCreate.members.first().voice);
+					memb.send(sorryMessage['ru']).catch();
 					if(this.channelCreate.members) 
 						this.channelCreate.members.forEach(
-							memb => {memb.voice.setChannel(channel).catch(reason => console.warn(reason));}
+							memb => {
+								memb.voice.setChannel(channel).catch(reason => console.warn(reason));
+								memb.send(sorryMessage['ru']).catch();
+							}
 						)
 				};
 				return;
