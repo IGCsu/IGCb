beaed308bf4204de36a03e543ab05bf240d03a27
BVN4 <ruslan2804y@gmail.com>
2022-06-03 10:44:43 +0300

    Update fag

--- commands/fag/index.js
+++ commands/fag/index.js
@@ -1,49 +1,46 @@
-﻿const sleep = require('../../functions/sleep');
 const { title, description } = require('./about.json');
-module.exports = {
 
+module.exports = {
 
 	active: true,
 	category: 'nsfw',
 
-
-	name: 'pidor',
+	name: 'fag',
 	title: title,
 	description: description,
 
 
-	init: function () { return this; }, 
+	init: function () { return this; },
+
 	/**
-	 * out - функция, которая отправляет заданное сообщение с задержкой в 2 секунды
-	 * @param {CommandInteraction} int 
-	 * @param {String} string 
+	 * Оправляет заданное сообщение с задержкой в 2 секунды
+	 * @param {CommandInteraction} int
+	 * @param {String}             string
 	 */
 	out: async function(int, string){
 		await int.channel.sendTyping();
 		await sleep(2000);
-		await int.editReply({
-			content: string
-		})
+		await int.editReply({ content: string });
 	},
 
-
-	call: async function (int) {
-		await int.reply({
-			content: 'Игогооооооо!'
-		})
+	/**
+	 * Пингует случайного пользователя
+	 * @param {CommandInteraction} int
+	 */
+	call: async function(int){
+		await int.reply({ content: 'Игогооооооо!' });
 		await this.out(int, 'Думаю...');
 		await this.out(int, 'А пониеб-то у нас:');
 		await int.followUp({
-			content: int.guild.members.cache.random().tag + '!'
+			content: int.guild.members.cache.random() + '!'
 		})
 	},
 
 	/**
 	 * Функция обработки slash-команды.
-	 * Если функция существует - то при инициализации будет добавлена слеш-команда с данными модуля и slashOptions, если тот имеется.
 	 * @param {CommandInteraction} int
 	 */
-	slash: async function (int) { 
+	slash: async function(int){
 		await this.call(int);
 	}
 }
