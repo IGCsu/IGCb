0028e9f4366455e697e73c6161c95ee1bf183af3
BVN4 <ruslan2804y@gmail.com>
2022-08-09 18:44:00 +0300

    Create GuildMember.toName.js

---
+++ functions/GuildMember.toName.js
@@ -0,0 +1,25 @@
+const { GuildMember } = require('discord.js');
+
+/**
+ * Формирует имя участника гильдии
+ *
+ * @param {boolean} [discriminator=false] Если true - добавляет к нику дискриминатор
+ * @param {boolean} [id=false] Если true - добавляет к результату ID
+ * @return {string} Имя участника
+ */
+GuildMember.prototype.toName = function(discriminator, id){
+	let str = this.displayName ?? this.nickname ?? this.user.username;
+
+	if(discriminator){
+		str += '#' + this.user.discriminator;
+	}
+
+	if(id){
+		if(this.user.bot){
+			str = 'bot:' + str;
+		}
+		str = this.user.id + ':' + str;
+	}
+
+	return str;
+}
