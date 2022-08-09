d7751c3b2ee18a4987dda884fe39bcb49dc50bc0
BVN4 <ruslan2804y@gmail.com>
2022-08-09 15:07:07 +0300

    Create init protection.js

---
+++ init/protection.js
@@ -0,0 +1,39 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+
+/**
+ * Массив ID защищённых к запуску клиентов.
+ * @type {string[]}
+ */
+const protectedClients = [
+	'921385200162840596', // IGCb#3156
+];
+
+/**
+ * Посредник запуска бота. Требует подтверждение при попытке запуска на защищённом клиенте.
+ * @return {Promise<unknown>}
+ */
+module.exports = async function(){
+
+	if(!process.env.DEVELOPER) return;
+
+	if(protectedClients.indexOf(client.user.id) === -1) return;
+
+	console.log('\x1b[41m Попытка запуска защищённого клиента ' + client.user.username + '#' + client.user.discriminator + ' ');
+
+	return new Promise(resolve => {
+		rl.question(' Продолжить запуск? (y/n) \x1b[0m', answer => {
+			if(answer === 'y'){
+				rl.close();
+				resolve();
+			}else{
+				client.destroy();
+				process.exit(0);
+			}
+		});
+	});
+}
