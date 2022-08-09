cd8554f9b4cafebb85d2c1c5b33236c908f119db
BVN4 <ruslan2804y@gmail.com>
2022-08-09 17:48:18 +0300

    Create String.random.js

---
+++ functions/String.random.js
@@ -0,0 +1,18 @@
+const chars = 'abdehkmnpswxzABDEFGHKMNPQRSTWXZ123456789-';
+
+/**
+ * Возвращает рандомную строку
+ *
+ * @param {number} len Необходимая длина строки
+ * @return {string}
+ */
+String.random = len => {
+	let str = '';
+
+	for(let i = 0; i < len; i++){
+		let pos = Math.floor(Math.random() * chars.length);
+		str += chars[pos];
+	}
+
+	return str;
+}
