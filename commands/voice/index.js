0db59a5aeafb68972afceabc7559e26afc74ce67
TheKing-OfTime <artem.matvienko0@gmail.com>
2022-09-20 01:39:21 +0300

    Migrate voice to new commands structure

--- commands/voice/index.js
+++ commands/voice/index.js
@@ -1,85 +1,85 @@
-const slashOptions = require('./slashOptions.json');
-const { title, sorryMessage } = require('./about.json');
+const SlashOptions = require('../../BaseClasses/SlashOptions');
+const BaseCommand = require('../../BaseClasses/BaseCommand');
+const LangSingle = require('../../BaseClasses/LangSingle');
 const { VoiceState, CommandInteraction, UserContextMenuInteraction, VoiceChannel } = require('discord.js');
 
-module.exports = {
+const slashOptions = require('./slashOptions');
+const { title, sorryMessage } = require('./about.json');
 
-	active : true,
-	category : 'Голосовые каналы',
+class Voice extends BaseCommand{
 
-	name : 'voice',
-	title : title,
-	slashOptions : slashOptions,
+	constructor(path) {
+		super(path);
 
-	permission : {
-		MANAGE_CHANNELS : true,
-		DEAFEN_MEMBERS : true,
-		MUTE_MEMBERS : true,
-		MOVE_MEMBERS : true,
-		VIEW_CHANNEL : true,
-		MANAGE_ROLES : true,
-		CONNECT : true,
-		STREAM : true,
-		SPEAK : true,
-	},
+		this.category = 'Голосовые каналы'
+		this.name = 'voice'
+		this.title = title
+		this.slashOptions = slashOptions
 
-	/**
-	* Инициализирует прослушку необходимых ивентов.
-	* Находит категорию голосовых и #Создать канал. Создаёт его, если не находит.
-	*
-	* @return {Object}
-	*/
-	init : async function(path){
-		guild.channels.cache.forEach(async c => {
-			if(c.type !== 'GUILD_VOICE' && c.type !== 'GUILD_CATEGORY') return;
-			if(c.name === 'Создать канал') {
-				this.channelCreate = c;
-				const mbr = this.channelCreate.members.filter(m => !m.user.bot).first();
-				if(this.channelCreate.members.filter(m => !m.user.bot).size && c.type === 'GUILD_VOICE'){
-					const channel = await this.create(mbr.voice);
-					if(this.channelCreate.members) 
-						this.channelCreate.members.forEach(
-							memb => {
-								memb.voice.setChannel(channel).catch(reason => console.warn(reason));
-								memb.send(sorryMessage['ru']).catch();
-							}
-						)
+		this.permission = {
+			MANAGE_CHANNELS : true,
+			DEAFEN_MEMBERS : true,
+			MUTE_MEMBERS : true,
+			MOVE_MEMBERS : true,
+			VIEW_CHANNEL : true,
+			MANAGE_ROLES : true,
+			CONNECT : true,
+			STREAM : true,
+			SPEAK : true,
+		}
+
+		return new Promise(async resolve => {
+
+			guild.channels.cache.forEach(async c => {
+				if (c.type !== 'GUILD_VOICE' && c.type !== 'GUILD_CATEGORY') return;
+				if (c.name === 'Создать канал') {
+					this.channelCreate = c;
+					const mbr = this.channelCreate.members.filter(m => !m.user.bot).first();
+					if (this.channelCreate.members.filter(m => !m.user.bot).size && c.type === 'GUILD_VOICE') {
+						const channel = await this.create(mbr.voice);
+						if (this.channelCreate.members)
+							this.channelCreate.members.forEach(
+								memb => {
+									memb.voice.setChannel(channel).catch(reason => console.warn(reason));
+									memb.send(sorryMessage['ru']).catch();
+								}
+							)
+					}
+					return;
 				}
-				return;
-			}
-			if(c.name === 'Голосовые') return this.channelCategory = c;
-			if(!c.members.filter(m => !m.user.bot).size && c.type === 'GUILD_VOICE') return this.delete(c, false, path);
-		});
-
-		if(!this.channelCategory){
-			log.initText += log.error(path + ': Отсутствует категория голосовых каналов');
-			this.active = false;
-			return this;
-		}
-
-		if(!this.channelCreate){
-			log.initText += log.warn(path + ': Отсутствует #Создать канал');
-			this.channelCreate = await guild.channels.create('Создать канал', {
-				parent : this.channelCategory.id,
-				type : 'GUILD_VOICE'
+				if (c.name === 'Голосовые') return this.channelCategory = c;
+				if (!c.members.filter(m => !m.user.bot).size && c.type === 'GUILD_VOICE') return this.delete(c, false, path);
 			});
-			log.initText += log.warn(path + ': Создан #Создать канал');
-		}
 
-		this.channelCreate.permissionOverwrites.cache.forEach(p => {
-			if(p.type === 'member') p.delete();
+			if(!this.channelCategory){
+				log.initText += log.error(path + ': Отсутствует категория голосовых каналов');
+				this.active = false;
+				return this;
+			}
+
+			if(!this.channelCreate){
+				log.initText += log.warn(path + ': Отсутствует #Создать канал');
+				this.channelCreate = await guild.channels.create('Создать канал', {
+					parent : this.channelCategory.id,
+					type : 'GUILD_VOICE'
+				});
+				log.initText += log.warn(path + ': Создан #Создать канал');
+			}
+
+			this.channelCreate.permissionOverwrites.cache.forEach(p => {
+				if(p.type === 'member') p.delete();
+			});
+
+			client.on('voiceStateUpdate', (before, after) => this.update(before, after));
+			resolve(this);
 		});
-
-		client.on('voiceStateUpdate', (before, after) => this.update(before, after));
-
-		return this;
-	},
+	}
 
 	/**
 	 * 
 	 * @param {CommandInteraction} int 
 	 */
-	slash: async function(int){
+	async slash(int){
 		if(int.options.getSubcommand() === 'auto-sync'){
 			await int.deferReply({ephemeral: true});
 			DB.query(`UPDATE users SET mode = "${int.options.getString('mode')}" WHERE id = ${int.user.id};`)[0];
@@ -104,7 +104,7 @@ module.exports = {
 		} else {
 			await int.reply({content: int.str('In development'), ephemeral: true});
 		}
-	},
+	}
 
 
 
@@ -116,7 +116,7 @@ module.exports = {
 	 * @param {VoiceState} before Предыдущий канал
 	 * @param {VoiceState} after  Текущий канал
 	 */
-	update : async function(before, after){
+	async update(before, after){
 		const state = after.channel ? after : before;
 		if(state.guild.id !== guild.id) return;
 
@@ -138,11 +138,7 @@ module.exports = {
 		if(!before.channel || channel.before.id === this.channelCreate.id || before.channel.members.filter(m => !m.user.bot).size) return;
 
 		this.delete(before.channel, user);
-	},
-
-
-
-
+	}
 
 
 	/**
@@ -153,13 +149,13 @@ module.exports = {
 	 *
 	 * @param {VoiceState} data
 	 */
-	create : async function(data){
+	async create(data){
 		let preset
 		try{
 			preset = DB.query(`SELECT * FROM users WHERE id = '${data.member.id}';`)[0];
 		} catch(e){
 			console.log("DB error occurred:\n" + e)
-		};
+		}
 		if(preset) preset.voice_data = JSON.parse(preset.voice_data);
 		const name = (preset?.mode !== 0 ? preset?.voice_data?.name : undefined) ?? data.member.toName();
 
@@ -183,7 +179,7 @@ module.exports = {
 		this.channelCreate.permissionOverwrites.create(data.member, { CONNECT : false });
 		setTimeout(() => this.channelCreate.permissionOverwrites.delete(data.member), 60000);
 		return channel;
-	},
+	}
 
 
 
@@ -193,7 +189,7 @@ module.exports = {
 	 * Находит каналы, которые под владением автора команды и передаёт их указанному пользователю
 	 * @param {UserContextMenuInteraction} ctx
 	 */
-	/*contextUser : async function(ctx){
+	/*async contextUser(ctx){
 		let channels = [];
 
 		client.channels.cache.forEach(c => {
@@ -223,7 +219,7 @@ module.exports = {
 	 * @param {String}       user    Никнейм пользователя
 	 * @param {String}       path    Путь к модулю
 	 */
-	delete : async function(channel, user, path){
+	async delete(channel, user, path){
 		if(channel.name.slice(-1) === '*') return;
 
 		const func = path
@@ -231,26 +227,26 @@ module.exports = {
 			: () => log.info(user, 'delete', '#' + channel.name);
 
 		channel.delete().then(func, func);
-	},
+	}
 
 	/**
 	 * Загрузка канала в БД
 	 * @param {VoiceState} voice
 	 */
-	upload : async function(voice){
+	async upload(voice){
 		let voice_data = JSON.stringify({name: voice.channel.name, bitrate: voice.channel.bitrate, userLimit: voice.channel.userLimit})
 		if(DB.query(`SELECT * FROM users WHERE id = ${voice.member.user.id};`)[0]){
 			DB.query(`UPDATE users SET voice_data = ? WHERE id = ${voice.member.user.id};`, [voice_data])[0];
 		} else {
 			DB.query(`INSERT INTO users VALUES (?, ?, ?, ?);`, [voice.member.user.id, 0, voice.channelId, voice_data])[0];
 		}
-	},
+	}
 
 	/**
 	 * 
 	 * @param {VoiceState} voice 
 	 */
-	sync : async function(voice){
+	async sync(voice){
 		let voiceConfiguration = JSON.parse((DB.query(`SELECT * FROM users WHERE id = ${voice.member.user.id};`)[0]).voice_data);
 		if(!voiceConfiguration) return 'There is no data entry in the database associated with you. Use `/upload` to fix it.';
 
@@ -259,5 +255,6 @@ module.exports = {
 		await voice.channel.edit(voiceConfiguration, 'По требованию ' + voice.member.toName());
 		return 0;
 	}
+}
 
-};
+module.exports = Voice;
