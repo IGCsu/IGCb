cac6daea82bd9edfa6d1fc75eb0ea5aab8c88fa0
TheKing-OfTime <artem.matvienko0@gmail.com>
2022-09-19 20:58:58 +0300

    Add static fields for SlashOption.type and .channel_types

--- BaseClasses/SlashOption.js
+++ BaseClasses/SlashOption.js
@@ -18,6 +18,36 @@ const SlashOptions = require('./SlashOptions');
  */
 class SlashOption {
 
+
+	static TYPES = {
+		SUB_COMMAND: 		1,
+		SUB_COMMAND_GROUP: 	2,
+		STRING: 			3,
+		INTEGER: 			4,
+		BOOLEAN: 			5,
+		USER: 				6,
+		CHANNEL: 			7,
+		ROLE: 				8,
+		MENTIONABLE: 		9,
+		NUMBER: 			10,
+		ATTACHMENT: 		11,
+	}
+
+	static CHANNEL_TYPES = {
+		GUILD_TEXT:			 0,
+		DM:					 1,
+		GUILD_VOICE: 		 2,
+		GROUP_DM:			 3,
+		GUILD_CATEGORY:		 4,
+		GUILD_ANNOUNCEMENT:	 5,
+		ANNOUNCEMENT_THREAD: 10,
+		PUBLIC_THREAD:		 11,
+		PRIVATE_THREAD:		 12,
+		GUILD_STAGE_VOICE:	 13,
+		GUILD_DIRECTORY:	 14,
+		GUILD_FORUM:		 15,
+	}
+
 	/**
 	 * Тип опции.
 	 * @see https://discord.com/developers/docs/interactions/application-commands#application-command-object-application-command-option-type
