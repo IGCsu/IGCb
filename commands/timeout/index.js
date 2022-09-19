77c45c732cb8f70cf3e432f7f5d569f7d8690680
TheKing-OfTime <artem.matvienko0@gmail.com>
2022-09-20 01:39:10 +0300

    Migrate timeout to new commands structure

--- commands/timeout/index.js
+++ commands/timeout/index.js
@@ -1,32 +1,38 @@
-const slashOptions = require('./slashOptions.json');
+const SlashOptions = require('../../BaseClasses/SlashOptions');
+const BaseCommand = require('../../BaseClasses/BaseCommand');
+const LangSingle = require('../../BaseClasses/LangSingle');
+const { GuildMember, AutocompleteInteraction, CommandInteraction, UserContextMenuInteraction, InteractionReplyOptions} = require('discord.js')
+
+const slashOptions = require('./slashOptions');
 const { title, description } = require('./about.json');
 const fetch = require("node-fetch");
 
-module.exports = {
+class Timeout extends BaseCommand{
 
-	active : true,
-	category : 'Модерация',
+	constructor(path) {
+		super(path);
 
-	name : 'timeout',
-	title : title,
-	description : description,
-	slashOptions : slashOptions,
+		this.category = 'Модерация'
 
+		this.name = 'timeout'
+		this.title = title
+		this.description = description
+		this.slashOptions = slashOptions
 
-	init : async function(path){
-		this.rulesCache = {};
-		try{
-			this.rules = await (await fetch(constants.SITE_LINK + '/rules?j=true')).json();
-			for(let rule in this.rules){
-				if(!rule.startsWith('a'))
-					this.rulesCache[rule] = this.rules[rule];
+		return new Promise(async resolve => {
+			this.rulesCache = {};
+			try{
+				this.rules = await (await fetch(constants.SITE_LINK + '/rules?j=true')).json();
+				for(let rule in this.rules){
+					if(!rule.startsWith('a'))
+						this.rulesCache[rule] = this.rules[rule];
+				}
+			}catch(e){
+				log.initText += log.warn(path + ': Site offline: Off generate suggestions');
 			}
-		}catch(e){
-			log.initText += log.warn(path + ': Site offline: Off generate suggestions');
-		}
-
-		return this;
-	},
+			resolve(this);
+		});
+	}
 
 
 	/**
@@ -36,13 +42,14 @@ module.exports = {
 	 * @param {GuildMember|Number}                            member Объект или ID пользователя
 	 * @param {String}                                        string Строка для парсинга врпемени
 	 * @param {String}                                        reason Причина
+	 * @return {InteractionReplyOptions}
 	 */
-	call : async function(int, member, string, reason){
+	async call(int, member, string, reason){
 		if(!this.permission(int.member))
-			return int.reply({
+			return {
 				content : reaction.emoji.error + ' ' + int.str('You do not have enough rights to change the roles of other users'),
 				ephemeral : true
-			});
+			};
 
 		let duration = 1;
 		try{
@@ -50,9 +57,9 @@ module.exports = {
 			duration = ((time.groups.days ?? 0) * 86400000) + ((time.groups.hours ?? 0) * 3600000) + ((time.groups.minutes ?? 0) * 60000) + (time.groups.seconds ?? 0) * 1000;
 		} catch(e) {
 			console.log(e);
-			return int.reply({ content : int.str('Invalid duration provided'), ephemeral : true});
+			return { content : int.str('Invalid duration provided'), ephemeral : true};
 		}
-		if(!duration || Math.floor(duration / 86400000) > 28) return int.reply({ content : int.str('Invalid duration provided'), ephemeral : true});
+		if(!duration || Math.floor(duration / 86400000) > 28) return { content : int.str('Invalid duration provided'), ephemeral : true};
 		await member.timeout(duration, reason);
 		return { embeds: [
 			new Discord.MessageEmbed()
@@ -60,14 +67,14 @@ module.exports = {
 				.setColor(2075752)
 		]};
 		
-	},
+	}
 
 
 	/**
 	 * Обработка слеш-команды
 	 * @param {CommandInteraction} int Команда пользователя
 	 */
-	slash : async function(int){
+	async slash(int){
 		let msg = '';
 		if (int.options.getString('reason') === 'SITE_OFFLINE')
 			msg = { contnet: 'Этот вариант не предусмотрен для выбора как причина мута, а всего лишь информирует о том, что бот не смог получить список всех правил.\nВпредь пожалуйста больше не выбирайте данный пункт', ephemeral: true }
@@ -75,14 +82,14 @@ module.exports = {
 			msg = await this.call(int, int.options.getMember('user'), int.options.getString('duration'), int.options.getString('reason') ?? '');
 		
 		await int.reply(msg);
-	},
+	}
 
 
 	/**
 	 * 
 	 * @param {AutocompleteInteraction} int 
 	 */
-	autocomplete : async function(int){
+	async autocomplete(int){
 		const rawReason = int.options.getFocused();
 		if(!rawReason) return
 		let choices = [{ name: rawReason, value: rawReason }];
@@ -110,16 +117,18 @@ module.exports = {
 			choices.push({ name: reason + ': ' + this.rulesCache[reason], value: rawReason })
 		}
 		return await int.respond(choices);
-	},
+	}
 
 	/**
 	 * Проверка наличия роли Сенат или Привратник
 	 *
 	 * @param {GuildMember} member
 	 */
-	permission : member =>
-		member.roles.cache.has('916999822693789718') ||
+	permission(member) {
+		return member.roles.cache.has('916999822693789718') ||
 		member.roles.cache.has('613412133715312641') ||
 		member.id === '500020124515041283'
+	}
+}
 
-};
+module.exports = Timeout;
