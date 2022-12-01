55d713f92b6637973cf25684a47ed87f3e3c2117
Ruslan <Ruslan2804Y@gmail.com>
2022-12-01 14:27:40 +0300

    Kladem hui na DB errors
    Никто не фиксит, заебало

--- commands/levels/UserLevels.js
+++ commands/levels/UserLevels.js
@@ -81,16 +81,22 @@ class UserLevels {
 	 * Обновляет данные пользователя в базе данных
 	 */
 	update () {
-		DB.query(
-			'UPDATE levels SET messagesAll = ?, messagesLegit = ?, symbols = ?, last = ? WHERE id = ?',
-			[
-				this.#primitiveData.messagesAll,
-				this.#primitiveData.messagesLegit,
-				this.#primitiveData.symbols,
-				this.#primitiveData.last,
-				this.member.id
-			]
-		);
+		// TODO: Модулю настала пизда, очень много флудит коннектами к БД. 
+		//  Надо сделать кеширование левелов и регулярную синхронизацию с БД. 
+		//  Пушто создание коннекта после каждого сообщения юзера - кладет БД.
+		//  На похуй будем ловить ошибки от базы, хуй с ней, если скипнем одно-два сообщения юзера
+		try {
+			DB.query(
+				'UPDATE levels SET messagesAll = ?, messagesLegit = ?, symbols = ?, last = ? WHERE id = ?',
+				[
+					this.#primitiveData.messagesAll,
+					this.#primitiveData.messagesLegit,
+					this.#primitiveData.symbols,
+					this.#primitiveData.last,
+					this.member.id
+				]
+			);
+		} catch (e) {}
 
 		return this;
 	};
