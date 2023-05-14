19aa0ab6e901a1e29eed4770539197f62220ccc7
Georgiy Belyaev <georgibeliaev@gmail.com>
2023-05-14 23:20:33 +0300

    Сделал как надо (наверное)

--- commands/handler/functions/pingPromoter.js
+++ commands/handler/functions/pingPromoter.js
@@ -1,34 +1,42 @@
+const MONITORING_BOT_ID = '464272403766444044';
+const COOLDOWN_UP = 4*3600*1000;
 module.exports = {
 
 	active: true,
 
 	title: {
-		ru: 'это не моих рук дело'
+		ru: 'Автоматическое упоминание людей, готовых продвигать сервер',
+		en: 'Automatic mention of people who are ready to promote the server',
+		uk: 'Автоматичне згадування людей, що готові просувати сервер'
 	},
 
-	allChannels: false,
+	allChannels: true,
 
 	allowedChannels: {
-		'610371610620198922': false, // #рандом
-		'949790593251414027': false // чат тест сервера
+		'610371610620198922': false // #рандом
 	},
 
 	init: async function () {
-		this.role = await guild.roles.fetch('айди роли');
+		this.role = await guild.roles.fetch('1107382381700206672');
 		return this;
 	},
 
 	call: async function (msg) {
-		if(msg.member.user.id !== '464272403766444044') return;
-		setTimeout(async () => {
-			const msgUpdated = await msg.channel.messages.fetch(msg.id);
-			const embed = msgUpdated.embeds[0];
-			if(!embed.description.includes('Успешный')) return;
-			console.log(embed)
-			setTimeout(async () => {
-				await msg.channel.send(this.role.toString())
-			 }, /* 1000 */ 4*3600*1000)
-		}, 1000)
-	}
+		if(msg.member.user.id !== MONITORING_BOT_ID) return;
 
+		let embed;
+		for(let i = 0; i < 3; i++){
+			await sleep(1000);
+
+			const msgUpdated = await msg.channel.messages.fetch(msg.id);
+			embed = msgUpdated.embeds[0];
+
+			if(embed) break;
+		}
+
+		if(!embed || !embed.description.includes('Успешный Up!')) return;
+
+		await sleep(COOLDOWN_UP)
+		await msg.channel.send(this.role.toString())
+	}
 };
