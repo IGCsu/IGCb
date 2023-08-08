e5d9366feeebbb5d8d17155e444b7829c02023db
TheKing-OfTime <artem.matvienko0@gmail.com>
2023-08-08 09:32:50 +0300

    Fix linux inconsistent

--- functions/getMilliseconds.js
+++ functions/getMilliseconds.js
@@ -2,6 +2,6 @@
  * Возвращает количество милисекунд с 3 цифрами после запятой с начала эпохи (01.01.1970)
  */
 global.getMilliseconds = function () {
-	const hrTime = process.hrtime();
-	return round(hrTime[0] * 1000000000 + hrTime[1] / 1000000, 9)
+	const hrTime = Number(process.hrtime.bigint());
+	return round(hrTime / 1000000, 9)
 }
