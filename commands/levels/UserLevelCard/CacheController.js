22843cc80708263ba3c0a637f936e40eaf7f77ef
TheKing-OfTime <artem.matvienko0@gmail.com>
2023-11-16 11:31:01 +0300

    debug

--- commands/levels/UserLevelCard/CacheController.js
+++ commands/levels/UserLevelCard/CacheController.js
@@ -4,30 +4,33 @@ const p = require('path')
 class CacheController {
 
 	#path = '';
+	#type = '';
 
 	constructor (path, type) {
 		this.#path = path.slice(0, -9) + "/UserLevelCard/cache/" + type
-		console.log(p.resolve(this.#path) + " Cache controller connected")
+		this.#type = '/' + type
+		console.log(p.resolve(this.#path) + " Cache controller connected");
+		this.clearAll();
 	}
 
 	async get(name) {
 		const endPath = this.#path + '/' + name;
 		let data = null;
-		console.log("Getting cached data from: " + endPath);
+		console.log("Getting cached data from: " + this.#type + '/' + name);
 		try {
 			data = await fs.readFile(endPath);
 		} catch (e) {
 			console.log("Failed to get cache");
 			return null;
 		}
-		console.log("Cache found")
+		console.log("Cache found");
 		return data;
 	}
 
 	async getAsJson(name) {
 		const endPath = this.#path + '/' + name + '.json';
 		let data = null;
-		console.log("Getting cached data from: " + endPath);
+		console.log("Getting cached data from: " + this.#type + '/' + name);
 
 		try {
 			data = JSON.parse(await fs.readFile(endPath, 'utf8'));
@@ -35,12 +38,12 @@ class CacheController {
 			console.log('Cache not found');
 			return null;
 		}
-		console.log("Cache found")
+		console.log("Cache found");
 		return data;
 	}
 
 	async set(name, file) {
-		console.log("Writing cached data to: " + this.#path + '/' + name)
+		console.log("Writing cached data to: " + this.#type + '/' + name)
 		console.log(file.length)
 		try {
 			await fs.writeFile(this.#path + '/' + name, file, {encoding: 'utf-8', })
@@ -57,10 +60,12 @@ class CacheController {
 	}
 
 	async clear(name) {
+		console.log('Clearing cache: ' + this.#type + ' ' + name)
 		await fs.rm(this.#path + '/' + name)
 	}
 
 	async clearAll() {
+		console.log('Clearing all caches: ' + this.#type)
 		const files = fs.readdir(this.#path)
 		for (file of files) {
 			console.log(file)
